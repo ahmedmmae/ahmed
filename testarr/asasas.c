cff2304a17ef6ab25483f8fe05a687875a5fcdd6
ahmedzima <G@G-PC>
2016-02-01 21:40:42 +0200

    add new project

---
+++ testarr/asasas.c
@@ -0,0 +1,18 @@
+/*
+ * asasas.c
+ *
+ *  Created on: ??þ/??þ/????
+ *      Author: G
+ */
+
+#include "stdio.h"
+#include "types.h"
+int main(void)
+{
+
+
+
+			printf("7amada");
+			return 0;
+
+			}
